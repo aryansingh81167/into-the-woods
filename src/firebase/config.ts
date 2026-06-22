cc9c42ae9c814fc244718c2916a8384f2043c90b
AryanSingh2k4 <aryanvsingh641@gmail.com>
2026-06-22 20:51:13 +0530

    Fix TypeScript errors in firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+/// <reference types="node" />
+
 export const firebaseConfig = {
-  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
-  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  measurementId: (process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "") as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string
 };
