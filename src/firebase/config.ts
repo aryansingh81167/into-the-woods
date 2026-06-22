c5329853b78a8bc721ba9dc15b7974ca1fd1f4ce
AryanSingh2k4 <aryanvsingh641@gmail.com>
2026-06-22 20:57:34 +0530

    dummy keys added

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 /// <reference types="node" />
 
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: (process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "demo-project") as string,
+  appId: (process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1234567890:web:abcdef123456") as string,
+  apiKey: (process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDummyKeyForFrontendDisplayOnly0") as string,
+  authDomain: (process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "demo-project.firebaseapp.com") as string,
   measurementId: (process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "") as string,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string
+  messagingSenderId: (process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1234567890") as string
 };
